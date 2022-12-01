6771e55cabdd764525ef35233a992322325ba547
E. G. Bland <liz@flompy.zone>
2022-12-01 22:00:00 +0000

    initial commit

---
+++ src-exe/AoC/Year2022/Day1.hs
@@ -0,0 +1,17 @@
+module AoC.Year2022.Day1 (day1) where
+
+import AoC.Prelude ( Problem(..) )
+import Data.List ( sort )
+import Data.List.Split ( splitOn )
+
+elfSums :: String -> [Int]
+elfSums = map (sum . map read . lines) . splitOn "\n\n"
+
+solveProblem1 :: String -> Int
+solveProblem1 = maximum . elfSums
+
+solveProblem2 :: String -> Int
+solveProblem2 = sum . take 3 . reverse . sort . elfSums
+
+day1 :: Problem
+day1 = Problem 1 solveProblem1 solveProblem2
