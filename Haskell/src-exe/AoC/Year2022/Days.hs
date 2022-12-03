3d059a74267649c2dfdb1734246ee43d35dd4a4f
E. G. Bland <liz@flompy.zone>
2022-12-03 10:19:01 +0000

    add some 2015 and up to day 3 of 2022

---
+++ Haskell/src-exe/AoC/Year2022/Days.hs
@@ -0,0 +1,9 @@
+module AoC.Year2022.Days ( days) where
+
+import AoC.Prelude ( Problem(..) )
+import AoC.Year2022.Day1 ( day1 )
+import AoC.Year2022.Day2 ( day2 )
+import AoC.Year2022.Day3 ( day3 )
+
+days :: [Problem]
+days = [day1,day2,day3]
