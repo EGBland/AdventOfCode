604cfc19b04d971b69458ab358235035a07f5650
Lizzy Bland <lizzy.bland@mht-technology.co.uk>
2022-12-06 13:13:55 +0000

    complete day 5 of 2022

--- src-exe/AoC/Year2022/Day5.hs
+++ src-exe/AoC/Year2022/Day5.hs
@@ -2,19 +2,32 @@ module AoC.Year2022.Day5 ( day5 ) where
 
 import AoC.Prelude ( Problem(..), dummy )
 import Data.Char ( isDigit )
-import Data.Map.Strict ( Map, (!), insert )
+import Data.Map.Strict ( Map, (!), insert, elems )
 import qualified Data.Map.Strict as M
 import Data.Maybe ( mapMaybe )
 
 day5 :: Problem
-day5 = Problem 2022 5 solveProblem1 dummy
+day5 = Problem 2022 5 solveProblem1 solveProblem2
 
 solveProblem1 :: String -> String
 solveProblem1 input =
     let
         (configLines, moveLines) = divvyFile input
         initialState = parseConfig configLines
-        moves = map parseMove moveLines
+        moves = map (parseMove True) moveLines
+        finalState = foldl (flip ($)) initialState moves
+    in
+        elems . fmap head $ finalState
+
+solveProblem2 :: String -> String
+solveProblem2 input =
+    let
+        (configLines, moveLines) = divvyFile input
+        initialState = parseConfig configLines
+        moves = map (parseMove False) moveLines
+        finalState = foldl (flip ($)) initialState moves
+    in
+        elems . fmap head $ finalState
 
 divvyFile :: String -> ([String],[String])
 divvyFile input =
@@ -32,8 +45,8 @@ parseConfig configLines =
     in
         M.fromList stackMap
 
-parseMove :: String -> Map Int [Char] -> Map Int [Char]
-parseMove input m =
+parseMove :: Bool -> String -> Map Int [Char] -> Map Int [Char]
+parseMove shouldReverse input m =
     let
         nStr = takeWhile isDigit . drop 5 $ input
         nLen = length nStr
@@ -41,10 +54,11 @@ parseMove input m =
         from = read . take 1 . drop (11 + nLen) $ input
         to = read . take 1 . drop (16 + nLen) $ input
 
+        moveFun = if shouldReverse then reverse else id
         sFromNextState = drop n $ m!from
-        sToNextState = (reverse . take n $ m!from) ++ (m!to)
+        sToNextState = (moveFun . take n $ m!from) ++ (m!to)
     in
-        (insert from sFromNextState) . (insert to sToNextState) $ m
+        insert from sFromNextState . insert to sToNextState $ m
 
 
 readStackSymbol :: Int -> String -> Maybe Char
