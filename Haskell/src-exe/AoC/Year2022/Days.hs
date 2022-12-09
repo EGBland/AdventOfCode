fe4cdb6cf9059cc22becce059f01865aa3cc4302
Lizzy Bland <lizzy.bland@mht-technology.co.uk>
2022-12-09 16:59:16 +0000

    add tree structure for day 7

--- Haskell/src-exe/AoC/Year2022/Days.hs
+++ Haskell/src-exe/AoC/Year2022/Days.hs
@@ -1,4 +1,4 @@
-module AoC.Year2022.Days ( days) where
+module AoC.Year2022.Days ( days ) where
 
 import AoC.Prelude ( Problem(..) )
 import AoC.Year2022.Day1 ( day1 )
@@ -7,6 +7,7 @@ import AoC.Year2022.Day3 ( day3 )
 import AoC.Year2022.Day4 ( day4 )
 import AoC.Year2022.Day5 ( day5 )
 import AoC.Year2022.Day6 ( day6 )
+import AoC.Year2022.Day7 ( day7 )
 
 days :: [Problem]
-days = [day1,day2,day3,day4,day5,day6]
+days = [day1,day2,day3,day4,day5,day6,day7]
