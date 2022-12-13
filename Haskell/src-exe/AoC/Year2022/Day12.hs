d19db4f55a11bfbcc478c90439c506ba0114c223
Lizzy Bland <lizzy.bland@mht-technology.co.uk>
2022-12-13 17:07:23 +0000

    add some digraph stuff

--- Haskell/src-exe/AoC/Year2022/Day12.hs
+++ Haskell/src-exe/AoC/Year2022/Day12.hs
@@ -2,7 +2,7 @@ module AoC.Year2022.Day12 ( day12 ) where
 
 import AoC.Prelude ( Problem(..), dummy )
 import AoC.Structures.GridMap ( coordifyGrid )
-import qualified Data.Map.Strict as M
 
 day12 :: Problem
 day12 = Problem 2022 12 dummy dummy
+
--- Haskell/src-exe/Main.hs
+++ Haskell/src-exe/Main.hs
@@ -4,7 +4,8 @@ import AoC.Prelude ( Problem(..), solveProblem )
 import AoC.Year2022.Days ( days )
 import Text.Printf ( printf )
 
-import AoC.Structures.Tree
+import AoC.Structures.Digraph ( Digraph, empty, addArc, adjacencyMatrix )
+import Data.Matrix ( prettyMatrix )
 
 printProblem :: Problem -> IO ()
 printProblem p@(Problem year day _ _) = do
@@ -12,4 +13,6 @@ printProblem p@(Problem year day _ _) = do
     printf "%d\t%d\t%s\t(%.3fs)\t%s\t(%.3fs)\n" year day p1 t1 p2 t2
 
 main :: IO ()
-main = mapM_ printProblem days
+main = mapM_ printProblem days >> do
+    let g = addArc empty 'a' 'b' 1
+    putStrLn . prettyMatrix . adjacencyMatrix $ g
